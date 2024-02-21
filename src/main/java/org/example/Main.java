fb1ae2d8350a4e25892d2715ff6ea27f5b5eb030
sofienneee <sofienne.mrabet@esprit.tn>
2024-02-22 00:19:02 +0100

    integration -1

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,32 +1,24 @@
 package org.example;
 
-import entities.partenaire;
-import service.partenaireService;
-import util.dataSource;
+import DataBaseSource.DataSource;
+import Entity.AdminUser.Admin;
+import Services.UserAdmineServices.AdminService;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
-import java.util.ArrayList;
-import java.util.List;
+import java.sql.Date;
 
-// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
-// then press Enter. You can now see whitespace characters in your code.
 public class Main {
-
-
     public static void main(String[] args) {
-        List<partenaire> list=new ArrayList<>();
-        partenaire p1=new partenaire("sofienne","type","dd",555,"@sof");
-        partenaire p2=new partenaire(2,"sofienne","type","dd",555,"@sof");
-        partenaireService p=new partenaireService();
-        //p.add(p1);
-        //p.addPst(p1);
-        //p.delete(p2.getId());
-        //p.update(p2);
-      // p.readAll().forEach(System.out::println);
-        System.out.println(p.readById(2));
+
+
+       // Admin admin=new Admin("Sofian","mrabet","sofiane.mrabet@esprit.tn","52225548",date,'H');
+        AdminService adminService =new AdminService();
+       // adminService.add(admin);
+        Admin admin1 =new Admin();
+        admin1=adminService.readById(2);
+        adminService.delete(admin1);
+
+
 
 
     }
-    }
+}
