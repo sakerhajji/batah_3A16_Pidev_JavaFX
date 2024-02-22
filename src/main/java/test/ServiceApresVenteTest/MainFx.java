ac16353e5f0be1c1301aa7613f6147d529cb08df
sofienneee <sofienne.mrabet@esprit.tn>
2024-02-22 23:17:13 +0100

    intergration sofienne 0

---
+++ src/main/java/test/ServiceApresVenteTest/MainFx.java
@@ -0,0 +1,45 @@
+package test.ServiceApresVenteTest;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+import javafx.stage.StageStyle;
+
+import java.io.IOException;
+
+public class MainFx extends Application {
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+private  double x,y  ;
+    @Override
+    public void start(Stage primaryStage) {
+        FXMLLoader loader=new FXMLLoader(getClass()
+                .getResource("/InterfaceUserAdmin/AccueilAdmin.fxml"));
+        try {
+            Parent root= loader.load();
+            Scene scene=new Scene(root);
+            primaryStage.initStyle(StageStyle.UNDECORATED);
+            primaryStage.setTitle("Inscription");
+            root.setOnMousePressed(event -> {
+                x = event.getSceneX();
+                y = event.getSceneY();
+            });
+            root.setOnMouseDragged(event -> {
+
+                primaryStage.setX(event.getScreenX() - x);
+                primaryStage.setY(event.getScreenY() - y);
+
+            });
+            primaryStage.setScene(scene);
+            primaryStage.initStyle(StageStyle.TRANSPARENT);
+            primaryStage.show();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+    }
+}
